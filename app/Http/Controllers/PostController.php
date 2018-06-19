0670cef47e03baeee690efb4b846c30e7c927624
juanitolaguna <little.pilgrim@mail.ru>
2018-06-20 01:37:48 +0200

    ...

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-    	return view('layouts.master');
+    	return view('posts.index');
     }
 }
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,15 +23,11 @@
 
 
 
-
-
-
-
     @include('layouts.nav')
 
     <div class="blog-header">
       <div class="container">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <h1 class="blog-title">The Bootstrap Blog!</h1>
         <p class="lead blog-description">An example blog template built with Bootstrap.</p>
       </div>
     </div>
@@ -40,7 +36,7 @@
 
       <div class="row">
 
-        @yield('content')
+        @yield('content')  
 
         @include('layouts.sidebar')
 
